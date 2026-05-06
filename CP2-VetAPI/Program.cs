a40ed98c511823ddfa8f5adb57dfbfd0dec3c111
gabriel <spbiel18@gmail.com>
2026-05-06 14:07:15 -0300

    removing views and correcting launchsettings

--- CP2-VetAPI/Program.cs
+++ CP2-VetAPI/Program.cs
@@ -4,40 +4,29 @@ using Microsoft.OpenApi.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 
-builder.Services.AddDbContext<ApplicationContext>(options =>
-{
+builder.Services.AddDbContext<ApplicationContext>(options => {
+
     options.UseOracle(builder.Configuration.GetConnectionString("Oracle"));
 });
 
-// Controllers
 builder.Services.AddControllers();
-
-// Swagger CORRETO
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen(c =>
-{
+builder.Services.AddSwaggerGen(c => {
     c.EnableAnnotations();
-
-    c.SwaggerDoc("v2", new OpenApiInfo
-    {
-        Title = "Vet API",
-        Version = "v2"
-    });
 });
 
 var app = builder.Build();
 
+// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
-    app.UseSwaggerUI(c =>
-    {
-        c.SwaggerEndpoint("/swagger/v2/swagger.json", "Vet API v2");
-    });
+    app.UseSwaggerUI();
 }
 
-app.UseHttpsRedirection();
 app.UseAuthorization();
+
 app.MapControllers();
 
 app.Run();
